ab2054c8dd6a32f65060c2966cc76fc00ea104bf
Jose Portilla <air_j_porti@icloud.com>
2023-05-10 18:30:19 +0200

    Added Model, Persistence and Model extension

---
+++ iFastFood/Persistence.swift
@@ -0,0 +1,19 @@
+//
+//  Persistence.swift
+//  iFastFood
+//
+//  Created by Jose Portilla Portilla on 10/5/23.
+//
+
+import Foundation
+
+final class Persistence {
+    static let shared = Persistence()
+    
+    private init() {}
+    
+    func fetchData() throws -> [MenuModel] {
+        return Bundle.main.decode(type: [MenuModel].self, from: "menu.json")
+
+    }
+}
